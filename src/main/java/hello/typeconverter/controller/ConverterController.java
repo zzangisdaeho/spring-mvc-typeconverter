401db96ed48df15a4b3c0dd9b4b0adfe3f82dcde
DaeHo Kim <zzangisdaeho@naver.com>
2022-03-04 00:37:00 +0900

    test api 추가

--- src/main/java/hello/typeconverter/controller/ConverterController.java
+++ src/main/java/hello/typeconverter/controller/ConverterController.java
@@ -4,9 +4,7 @@ import hello.typeconverter.type.IpPort;
 import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ConverterController {
@@ -39,5 +37,13 @@ public class ConverterController {
         public Form(IpPort ipPort) {
             this.ipPort = ipPort;
         }
+    }
+
+    // MyNumberFormatter를 등록한 덕분에 queryParam 으로 num=10,000 으로 보내도 정상 파싱 가능
+    @ResponseBody
+    @GetMapping("/formatter")
+    public String formatterTest(int num){
+        System.out.println(num);
+        return "OK";
     }
 }
